7ecf5c0c49c9d6eddc7b986bd99c829dc9a0d5b6
Danny Nhoisaykham <danny@MBP.local>
2017-08-24 23:06:34 -0500

    add scope-safe constructors

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,14 +1,18 @@
-var ClozeCard = function(text, cloze){
+var ClozeCard = function (text, cloze) {
+  // check if new function
+  if (!(this instanceof ClozeCard)) {
+    return new ClozeCard(text, cloze);
+  }
   // check if cloze in text
-  if(text.indexOf(cloze)<0){
-    return console.log("Error: ", cloze , " doesn't appear in ", text);
+  if (text.indexOf(cloze) < 0) {
+    return console.log('Error: ', cloze, " doesn't appear in ", text);
   }
 
   this.cloze = cloze;
   this.fullText = text;
-  
-  var regEx = new RegExp(cloze,"g");
-  this.partial = text.replace(regEx, "...");
-}
+
+  var regEx = new RegExp(cloze, 'g');
+  this.partial = text.replace(regEx, '...');
+};
 
 module.exports = ClozeCard;
