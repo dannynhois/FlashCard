79a76b832ffe483e16bd761ae43d4da55d79884b
Danny Nhoisaykham <danny@MBP.local>
2017-09-14 23:00:48 -0500

    working project

--- flashcard.js
+++ flashcard.js
@@ -5,6 +5,7 @@ var inquirer = require('inquirer');
 var clozeList = [];
 var basicList = [];
 
+seedCard();
 startProgram();
 
 function startProgram () {
@@ -107,24 +108,55 @@ function testCard () {
   });
 }
 
-function testClozeCard(){
+function testClozeCard () {
   var choices = [];
-  clozeList.forEach(card => {
-    choices.push(card.partial);
+  clozeList.forEach((card,index) => {
+    choices.push((index+1) + ") " +card.partial);
   });
 
   inquirer.prompt([
     {
       type: 'list',
-      name: 'typeOfCard',
+      name: 'card',
       message: 'Select flashcard to see answers:',
       choices: choices
     }
   ]).then(response => {
-    if (response.typeOfCard === 'Cloze') {
-      testClozeCard();
-    } else {
-      testBasicCard();
-    }
+    var index = parseInt(response.card.slice(0,response.card.indexOf(')')))-1;
+    console.log(clozeList[index].fullText);
+    startProgram();
   });
+}
+
+function testBasicCard () {
+  var choices = [];
+  basicList.forEach((card,index) => {
+    choices.push((index+1) + ") " +card.front);
+  });
+
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'card',
+      message: 'Select flashcard to see answers:',
+      choices: choices
+    }
+  ]).then(response => {
+    var index = parseInt(response.card.slice(0,response.card.indexOf(')')))-1;
+    console.log(basicList[index].back);
+    startProgram();
+  });
+}
+
+function seedCard () {
+  clozeList.push(new Cloze('JJ Watt is number 99 of the Texans', 'Texans'));
+  clozeList.push(new Cloze('James Harden plays for the Rockets', 'Rockets'));
+  clozeList.push(new Cloze('Jeff Bagwell played for the Astros', 'Astros'));
+  clozeList.push(new Cloze('Austin is the capital of Texas', 'Austin'));
+
+  basicList.push(new Basic('What is the capital of Texas', 'Austin'));
+  basicList.push(new Basic('Basketball team in Houston', 'Rockets'));
+  basicList.push(new Basic('Football team in Houston', 'Texans'));
+  basicList.push(new Basic('Baseball team in Houston', 'Astros'));
+  basicList.push(new Basic('Number 99 of the Texans', 'JJ Watts'));
 }
